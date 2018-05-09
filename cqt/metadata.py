2e483fc1212914217e23d1cc0743dd2f6c0b7fb6
Ang <ang@massles.ai>
2018-05-08 22:12:06 -0400

    Major structure changes. Updated the package name to CQT (Crypto Quant Trading)

---
+++ cqt/metadata.py
@@ -0,0 +1,11 @@
+package = 'cqt'
+project = "Crypto Quant Trader"
+version = '0.1.0'
+description = 'Crypto Quant Trader'
+author = ['Massless Inc.']
+emails = ['info@massless.ai']
+license = 'MIT'
+copyright = '2018 ' + author[0]
+url = 'http://massless.ai'
+
+# Obtain the above information from Setup.py
