c4975d62c6bdd57575b52d791aaaf645ed9cadb5
qianzuoming <qianzuoming@gmail.com>
2018-08-22 21:05:06 -0400

    ARRAY_OPS: implemented the basic 1d seres, spot, and scenario classes

--- array_ops/array_ops.py
+++ array_ops/array_ops.py
@@ -13,14 +13,14 @@ import numba
 
 @numba.jit
 def getMeanOfLastNElem(dataValueArray, n=1):
-	
-	'''
-	get the mean of last N elements from 1d array.
-	The function should be a common component of any 
-	moving average based signal indicators 
-	'''
+    
+    '''
+    get the mean of last N elements from 1d array.
+    The function should be a common component of any 
+    moving average based signal indicators 
+    '''
 
-	return np.mean(dataValueArray[-n:])
+    return np.mean(dataValueArray[-n:])
 
 
 
@@ -29,55 +29,55 @@ def getMeanOfLastNElem(dataValueArray, n=1):
 # a 90-30 indicator, or a 10-5 indicator
 @numba.jit
 def indicatorShorLongtWindowMean(dataValueArray, 
-									shortWindowWidth=1,
-									longWindowWidth=2):
-	'''
-	compare the average from long and short window
-	return:
-	* an indicator regarding who is bigger
-	* the difference shortWindowAvg - long windowAvg
-	'''
-	longWindowMean = getAbsoluteReturn(dataValueArray, longWindowWidth)
-	shortWindowMean = getAvgOfLastNElem(dataValueArray, shortWindowWidth)
-	diff = shortWindowMean - longWindowMean
-	if diff > 0:
-		return 1, diff
-	elif diff == 0:
-		return 0, diff
-	elif diff > 0:
-		return 1, diff
+                                 shortWindowWidth=1,
+                                 longWindowWidth=2):
+    '''
+    compare the average from long and short window
+    return:
+    * an indicator regarding who is bigger
+    * the difference shortWindowAvg - long windowAvg
+    '''
+    longWindowMean = getAbsoluteReturn(dataValueArray, longWindowWidth)
+    shortWindowMean = getAvgOfLastNElem(dataValueArray, shortWindowWidth)
+    diff = shortWindowMean - longWindowMean
+    if diff > 0:
+        return 1, diff
+    elif diff == 0:
+        return 0, diff
+    elif diff > 0:
+        return 1, diff
 
 @numba.jit
 def getAbsoluteReturn(dataValueArray, liquidityHorizon=1):
-	'''
-	b - a  operator
-	b and a index are off the by liquidity horizon integer
-	'''
-	aArray = dataValueArray[0:-liquidityHorizon]
-	bArray = dataValueArray[liquidityHorizon:]
-	shockArray =  bArray - aArray
-	return shockArray
+    '''
+    b - a  operator
+    b and a index are off the by liquidity horizon integer
+    '''
+    aArray = dataValueArray[0:-liquidityHorizon]
+    bArray = dataValueArray[liquidityHorizon:]
+    shockArray =  bArray - aArray
+    return shockArray
 
 @numba.jit
 def getRelativeReturn(dataValueArray, liquidityHorizon=1):
 
-	'''
-	b / a  operator
-	b and a index are off the by liquidity horizon integer
-	'''
-	aArray = dataValueArray[0:-liquidityHorizon]
-	bArray = dataValueArray[liquidityHorizon:]
-	shockArray =  np.divide(bArray, aArray)
-	return shockArray
+    '''
+    b / a  operator
+    b and a index are off the by liquidity horizon integer
+    '''
+    aArray = dataValueArray[0:-liquidityHorizon]
+    bArray = dataValueArray[liquidityHorizon:]
+    shockArray =  np.divide(bArray, aArray)
+    return shockArray
 
 @numba.jit
 def getLogReturn(dataValueArray, liquidityHorizon=1):
 
-	'''
-	log(b/a)  operator
-	b and a index are off the by liquidity horizon integer
-	'''
-	aArray = dataValueArray[0:-liquidityHorizon]
-	bArray = dataValueArray[liquidityHorizon:]
-	shockArray =  np.log(np.divide(bArray, aArray))
-	return shockArray
+    '''
+    log(b/a)  operator
+    b and a index are off the by liquidity horizon integer
+    '''
+    aArray = dataValueArray[0:-liquidityHorizon]
+    bArray = dataValueArray[liquidityHorizon:]
+    shockArray =  np.log(np.divide(bArray, aArray))
+    return shockArray
--- array_ops/time_series_objs.py
+++ array_ops/time_series_objs.py
@@ -7,15 +7,141 @@ Define basic time series objects here that utilizes the array_ops
 import numpy as np
 import pandas as pd
 import numba
+import array_ops as aop
+
+
+class Base1DSeries:
+    def __init__(self,
+                 dataValueArray=None,
+                 indexValueArray=None,
+                 name=None,
+                 metaDataDict=None):
+        
+        self.__dataValueArray = dataValueArray
+        self.__indexValueArray = indexValueArray
+        self.__name = name
+        self.__metaDataDict = metaDataDict
+    
+    def getDataValues(self):
+        return self.__dataValueArray
+
+    def setDataValues(self, dataValueArray):
+        self.__dataValueArray = dataValueArray
+
+    def getIndexValues(self):
+        return self.__indexValueArray
+
+    def setIndexValues(self, indexValueArray):
+        self.__indexValueArray = indexValueArray
+
+    def getName(self):
+        return self.name
+
+    def setName(self, name):
+        self.__name = name
+
+    def getMetaDataDict(self):
+        return self.__metaDataDict
+
+    def setMetaDataDict(self, metaDataDict):
+        self.__metaDataDict = metaDataDict
+
+    def toDataFrame(self);
+        df = pd.DataFrame()
+        df['index_value'] = self.__indexValueArray
+        df[self.__name] = self.__dataValueArray
+
+
+class ScenSeries(Base1DSeries):
+    def __init__(self,
+                 dataValueArray=None,
+                 indexValueArray=None,
+                 name=None,
+                 metaDataDict=None,
+                 liquidityHorizon=None,
+                 scenType=None):
+        
+        self.__dataValueArray = dataValueArray
+        self.__indexValueArray = indexValueArray
+        self.__name = name
+        self.__metaDataDict = metaDataDict
+        self.__liquidityHorizon = liquidityHorizon
+        self.__scenType = scenType
+
+    def getScenType(self):
+        return self.__scenType
+
+    def getLiquidityHorizon(self):
+        return self.__liquidityHorizon
+
+class SpotSeries(Base1DSeries):
+    
+    def getAbsoluteReturnArray(self, 
+                               liquidityHorizon=1):
+        returnArray = aop.getAbsoluteReturn(self.__dataValueArray,
+                                            liquidityHorizon=liquidityHorizon)
+        return returnArray
+
+    def getRelativeReturnArray(self, 
+                               liquidityHorizon=1):
+        returnArray = aop.getAbsoluteReturn(self.__dataValueArray,
+                                            liquidityHorizon=liquidityHorizon)
+        return returnArray
+    
+    def getLogReturnArray(self, 
+                           liquidityHorizon=1): 
+        returnArray = aop.getLogReturn(self.__dataValueArray,
+                                       liquidityHorizon=liquidityHorizon)
+        return returnArray
+
+    def getAbsoluteScen(self, liquidityHorizon=1):
+        
+        shockDataValueArray = self.getAbsoluteReturnArray(liquidityHorizon)
+        shockIndexValueArray = self.__indexValueArray[liquidityHorizon:]
+        scen = ScenSeries(shockDataValueArray, 
+                          shockIndexValueArray,
+                          name=self.__name,
+                          liquidityHorizon=liquidityHorizon,
+                          scenType ='absolute')    
+        return scen
+
+    def getRelativeScen(self, liquidityHorizon=1):
+        
+        shockDataValueArray = self.getRelativeReturnArray(liquidityHorizon)
+        shockIndexValueArray = self.__indexValueArray[liquidityHorizon:]
+        scen = ScenSeries(shockDataValueArray, 
+                          shockIndexValueArray,
+                          name=self.__name,
+                          liquidityHorizon=liquidityHorizon,
+                          scenType ='relative')    
+        return scen
+
+    def getLogReturnScen(self, liquidityHorizon=1):
+        
+        shockDataValueArray = self.getLogReturnArray(liquidityHorizon)
+        shockIndexValueArray = self.__indexValueArray[liquidityHorizon:]
+        scen = ScenSeries(shockDataValueArray, 
+                          shockIndexValueArray,
+                          name=self.__name,
+                          liquidityHorizon=liquidityHorizon,
+                          scenType ='log_return')    
+        return scen
+
+    def getMeanOfLastNElem(self, n=1):
+        return aop.getMeanOfLastNElem(self.__dataValueArray, 
+                                      n=n)
+
+    def indicatorShorLongtWindowMean(self, 
+                                     shortWindowWidth=1,
+                                     longWindowWidth=2)
+
+        return aop.indicatorShorLongtWindowMean(self.__dataValueArray,
+                                                shortWindowWidth=shortWindowWidth,
+                                                longWindowWidth=longWindowWidth)
+        
 
 
 
-class SpotSeries:
-	def __init__(self):
-		# self.__dataValueArray
-		# self.__indexValueArray
-		pass
-
-class OCLHVSeries:
-	def __init__(self):
-		pass
+# class OCLHVSeries:
+#     def __init__(self):
+#         pass
